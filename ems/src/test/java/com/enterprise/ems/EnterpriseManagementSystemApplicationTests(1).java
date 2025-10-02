bdcc0386611440dd1b5f26a5b4ecaaa3210ec4af
Csaht <ahteshamm876@gmail.com>
2025-10-02 23:12:10 +0530

    Initial commit

---
+++ ems/src/test/java/com/enterprise/ems/EnterpriseManagementSystemApplicationTests(1).java
@@ -0,0 +1,13 @@
+package com.enterprise.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
